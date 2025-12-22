1469e4141333e36c57f04aef4719c55c45d91001
dairi256 <72542198+dairi256@users.noreply.github.com>
2025-12-22 17:45:29 +0800

    Add files controller and generate swagger webpage
    Added FilesController in Controllers folder to handle the uploading. 'Program' also was edited to ensure that when using the webpage, it shows the swagger UI.

---
+++ UbuntuFileSystemAPI/Controllers/FilesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UbuntuFileSystemAPI.Services;
+
+namespace UbuntuFileSystemAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController(IFileService fileService) : ControllerBase
+    {
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if (file == null || file.Length == 0) // Basic validation if a file has been uploaded
+            {
+                return BadRequest("No file has been uploaded");
+            }
+
+            try
+            {
+                var fileName = await fileService.SaveFileAsync(file);
+
+                return Ok(new { Message = "Upload successful!", FileName = fileName });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+    }
+}
--- UbuntuFileSystemAPI/Program.cs
+++ UbuntuFileSystemAPI/Program.cs
@@ -7,22 +7,20 @@ namespace UbuntuFileSystemAPI
         {
             var builder = WebApplication.CreateBuilder(args);
 
-
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
             builder.Services.AddControllers();
-            builder.Services.AddOpenApi();
 
             builder.Services.AddScoped<Services.IFileService, Services.LocalFileService>();
             var app = builder.Build();
 
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
+            // app.UseHttpsRedirection();
 
             app.MapControllers();
 
