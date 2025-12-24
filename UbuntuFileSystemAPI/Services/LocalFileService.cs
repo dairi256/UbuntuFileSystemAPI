613249a69a4703e928e103085d1f914b15ccf9cf
dairi256 <72542198+dairi256@users.noreply.github.com>
2025-12-24 10:55:02 +0800

    Add database logic including saving and deleting

--- UbuntuFileSystemAPI/Services/LocalFileService.cs
+++ UbuntuFileSystemAPI/Services/LocalFileService.cs
@@ -1,14 +1,28 @@
 ﻿using System.Runtime.CompilerServices;
+using UbuntuFileSystemAPI.Data;
+using UbuntuFileSystemAPI.Models;
 
 namespace UbuntuFileSystemAPI.Services
 {
-    public class LocalFileService(IConfiguration config) : IFileService
+    public class LocalFileService: IFileService
     {
-        private readonly string _storagePath = config["FileStorage:StoragePath"]
-                                               ?? throw new Exception("Your storage path has not been configured.");
+        private readonly string _storagePath;
+        private readonly AppDbContext _context;
+
+        public LocalFileService(IConfiguration config, AppDbContext context)
+        {
+            _storagePath = config["StoragePath"] ?? "CloudStorage";
+            _context = context;
+
+            if (!Directory.Exists(_storagePath)) Directory.CreateDirectory(_storagePath);
+        }
 
         public async Task<string> SaveFileAsync(IFormFile file)
         {
+
+            var untrustedFilename = Path.GetFileName(file.FileName);
+            var storedFilename = $"{Guid.NewGuid()}_{untrustedFilename}";
+
             if (!Directory.Exists(_storagePath)) // Important if the directory doesn't exist
             {
                 Directory.CreateDirectory(_storagePath);
@@ -20,7 +34,17 @@ namespace UbuntuFileSystemAPI.Services
             using var stream = new FileStream(fullPath, FileMode.Create);
             await file.CopyToAsync(stream);
 
-            return trustedFilename;
+            var fileRecord = new FileRecord
+            {
+                FileName = storedFilename,
+                OriginalName = untrustedFilename,
+                SizeInBytes = file.Length,
+                UploadDate = DateTime.UtcNow
+            };
+
+            _context.Files.Add(fileRecord);
+            await _context.SaveChangesAsync(); // Pushes data to database
+            return storedFilename;
         }
 
         public IEnumerable<string> ListFiles()
@@ -50,6 +74,13 @@ namespace UbuntuFileSystemAPI.Services
             if (File.Exists(filePath))
                 {
                 File.Delete(filePath);
+
+                var record = _context.Files.FirstOrDefault(f => f.FileName == fileName);
+                if (record != null)
+                {
+                    _context.Files.Remove(record);
+                    _context.SaveChanges();
+                }
                 return true;
             }
 
