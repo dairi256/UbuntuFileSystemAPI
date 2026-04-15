bb5b153997dc660d454068957bba0f11a73f7930
dairi256 <derekingham25@outlook.com>
2026-04-15 19:09:19 +0800

    Update dependencies and create User Model - Updated dependencies for JWT - Create User.cs as an Auhtentication model which will later be used for token-based authentication (Will be implemented fully over the course of commits)

---
+++ UbuntuFileSystemAPI/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace UbuntuFileSystemAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; }
+        public DateTime? LastLogin { get; set; }
+    }
+}
